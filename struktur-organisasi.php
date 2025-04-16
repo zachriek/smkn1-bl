916c5622ebccfb4015666b82d6407e83d92eacd4
saif0606 <saifabdullah0509@gmail.com>
2025-04-16 20:05:26 +0700

    saif

--- struktur-organisasi.php
+++ struktur-organisasi.php
@@ -9,23 +9,23 @@
     <title>Struktur Organisasi</title>
 </head>
 
-<body class="flex flex-col min-h-screen bg-gray-50">
+<body class="bg-gray-50 flex flex-col min-h-screen">
 
     <?php include_once "components/header.php"; ?>
 
     <!-- Struktur Organisasi -->
     <main class="flex-grow">
         <section class="text-center py-12 mt-[80px]">
-            <h2 class="text-3xl md:text-4xl font-bold text-green-700">Struktur Organisasi</h2>
+            <h2 class="text-4xl font-bold text-green-700">Struktur Organisasi</h2>
             <p class="mt-4 text-lg text-gray-600">Struktur Organisasi SMK Negeri 1 Bandar Lampung</p>
 
-            <div class="container mx-auto flex flex-col items-center mt-8 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-2xl">
-                <div class="bg-green-700 text-white font-semibold text-xl py-3 px-6 rounded-lg w-full text-center mb-3">Ketua Yayasan</div>
-                <div class="bg-green-600 text-white font-semibold text-xl py-3 px-6 rounded-lg w-full text-center mb-3">Kepala Sekolah</div>
-                <div class="bg-green-500 text-white font-semibold text-xl py-3 px-6 rounded-lg w-full text-center mb-3">Waka Kurikulum</div>
-                <div class="bg-green-400 text-white font-semibold text-xl py-3 px-6 rounded-lg w-full text-center mb-3">Waka Kesiswaan</div>
-                <div class="bg-green-300 text-white font-semibold text-xl py-3 px-6 rounded-lg w-full text-center mb-3">Bendahara</div>
-                <div class="bg-green-200 text-black font-semibold text-xl py-3 px-6 rounded-lg w-full text-center">Dewan Guru</div>
+            <div class="container mx-auto flex flex-col items-center mt-10 bg-white p-8 rounded-2xl shadow-2xl w-full max-w-2xl">
+                <div class="bg-green-700 text-white font-semibold text-xl py-3 px-4 rounded-lg w-full text-center mb-3">Ketua Yayasan</div>
+                <div class="bg-green-600 text-white font-semibold text-xl py-3 px-4 rounded-lg w-full text-center mb-3">Kepala Sekolah</div>
+                <div class="bg-green-500 text-white font-semibold text-xl py-3 px-4 rounded-lg w-full text-center mb-3">Waka Kurikulum</div>
+                <div class="bg-green-400 text-white font-semibold text-xl py-3 px-4 rounded-lg w-full text-center mb-3">Waka Kesiswaan</div>
+                <div class="bg-green-300 text-white font-semibold text-xl py-3 px-4 rounded-lg w-full text-center mb-3">Bendahara</div>
+                <div class="bg-green-200 text-black font-semibold text-xl py-3 px-4 rounded-lg w-full text-center">Dewan Guru</div>
             </div>
         </section>
     </main>
@@ -34,6 +34,7 @@
 
     <!-- Script -->
     <script src="./js/script.js"></script>
+
 </body>
 
 </html>
